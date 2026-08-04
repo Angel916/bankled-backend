e0c0d1eafc917067ca87808d9aa09ec75d0497c6
angel jaiswal <angel@example.com>
2026-08-05 00:39:59 +0530

    Enable password reset OTP email

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -57,7 +57,7 @@ async function forgetPassword(req, res) {
     });
     console.log("otp",otp)
 
-    //await emailService.sendPasswordResetOTP(email, otp);
+    await emailService.sendPasswordResetOTP(email, otp);
     return res.status(200).json({
       message: "OTP sent successfully",
     });
