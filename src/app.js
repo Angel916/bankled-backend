b9f29277fb913803db8905d15868e02402995e9a
angel jaiswal <angel@example.com>
2026-08-05 23:21:27 +0530

    add root route

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ const cookieparser = require("cookie-parser")
 app.use(express.json())
 app.use(cookieparser())
 
+app.get("/",(req,res)=>{
+    res.send("Welcome to the banking app")
+})
+
 app.use("/api/auth",authRoutes)
 app.use("/api/accounts",accountRoutes)
 app.use("/api/transaction",transactionRoutes)
